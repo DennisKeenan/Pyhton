eb3ca04921091d8fa58297de24ee1e67c4f81070
DennisKeenan <dennis.k.arkananta@gmail.com>
2022-10-23 10:28:49 +0700

    Hobby and Destination Question

--- Introduction.py
+++ Introduction.py
@@ -1,4 +1,8 @@
-print("Hello World")
-name=input("What's your name?")
-print("Hello",name,sep=":")
-print(123,12.3)
+from re import T
+
+
+# print("Hello World")
+# name=input("What's your name?")
+# print("Hello",name,sep=":",end="\t")
+# print(123,12.3)
+print(type("Hello World"),type(123456),type(3.0),type(True))
