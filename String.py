02efe15f8fd580dab41bd3658743652fbeb65345
DennisKeenan <dennis.k.arkananta@gmail.com>
2022-11-19 10:09:51 +0700

    LotterString

---
+++ String.py
@@ -0,0 +1,26 @@
+text="hello world"
+# print(text.capitalize()) #Capitalize First Letter
+
+text1="HELLO WORLD WORLD "
+# print(text1.casefold()) #Lowercase All Letter in String
+
+# print(text1.center(150,"*")) #Title (Padding with specific charachter and number of characther)
+
+# print(text1.count("WORLD")) #Counting Words or Letter
+
+# print(text1.endswith(" "))
+
+# print(text1.find("WORLD")) #Finding world or letter by an index
+# if (text.find("WORLD")):
+#     print("World found in the string")
+
+password="Text Testing" 
+#Numeric, Alphabet, and Digit Checker
+# print(password.isalnum())
+# print(password.isalpha())
+# print(password.isdigit())
+
+#Lowercase and Uppercase Checker
+print(password.islower())
+print(password.isupper())
+print(password.istitle())
