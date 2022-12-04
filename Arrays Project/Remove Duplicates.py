b7016e146367c99490f0e674b8b86a34b4f910e0
DennisKeenan <dennis.k.arkananta@gmail.com>
2022-12-04 11:03:45 +0700

    Remove Duplicates

--- Arrays Project/Remove Duplicates.py
+++ Arrays Project/Remove Duplicates.py
@@ -1,14 +1,9 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         length=len(nums)
-        filterlist=[]
-        k=1
-        for i in range(length-1):
-            if (nums[i]!=nums[i+1]):
+        k=0
+        for i in range(1,length):
+            if (nums[i]!=nums[i-1]):
                 k+=1
-                filterlist.append(nums[i])
-                if i+1==length-1:
-                    filterlist.append(nums[i+1])
-        nums=filterlist
-        print(nums)
-        return(k)
+                nums[k]=nums[i]
+        return(k+1)
