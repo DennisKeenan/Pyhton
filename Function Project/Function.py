027405da2eac43a93d7f4edd0feab9e8287a3d49
DennisKeenan <dennis.k.arkananta@gmail.com>
2022-12-11 11:19:04 +0700

    Function

---
+++ Function Project/Function.py
@@ -0,0 +1,46 @@
+from turtle import *
+from time import sleep
+
+def drawCircle(quality:int=4,color="black",size:int=10):
+    speed(0)
+    fillcolor(color)
+    begin_fill()
+    for i in range(int(360/quality)):
+        forward(size)
+        left(quality)
+    end_fill()
+    sleep(3)
+
+def drawRectangle(color="maroon",length=50,width=70):
+    speed(1)
+    fillcolor(color)
+    begin_fill()
+    forward(width)
+    right(90)
+    forward(length)
+    right(90)
+    forward(width)
+    right(90)
+    forward(length)
+    right(90)
+    end_fill()
+    sleep(5)
+    return mul(length,width)
+
+def add(numberA,numberB):
+    return numberA+numberB
+
+def sub(numberA,numberB):
+    return numberA-numberB
+
+def mul(numberA,numberB):
+    return numberA*numberB
+
+def div(numberA,numberB):
+    return numberA/numberB
+
+def factorial(numberA):
+    result=1
+    for i in range(2,numberA+1):
+        result=mul(result,i)
+    return result
---
+++ Function Project/Main.py
@@ -0,0 +1,9 @@
+from Function import *
+
+# drawCircle()
+# drawCircle(6,"#00FF00")
+# drawCircle(6,"red",20)
+
+# print(drawRectangle())
+
+print(factorial(6))
